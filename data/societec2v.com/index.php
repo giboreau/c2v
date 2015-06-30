d908af760451ea824a9b52ecf2339163de269bca
Madeny <madeny@live.com>
2015-06-30 09:19:07 +0200

    init
    developing new bootstrap html

---
+++ data/societec2v.com/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<META NAME="DESCRIPTION" CONTENT="Accompagnement, formation, audit en paie, ressources humaines et protection sociale">
+		<link rel="icon" type="image/png" href="/images/Favicon.ico" />
+		<META NAME="ROBOTS" CONTENT="All">
+<link rel="icon" type="image/png" href="images/favicon.png" />
+<title>Société C2V - Formations</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="js/css/start/jquery-ui-1.8.11.custom.css">
+<script type="text/javascript" src="js/js/jquery-1.5.1.min.js"></script>
+<script type="text/javascript" src="js/js/jquery-ui-1.8.11.custom.min.js"></script>
+<script type="text/javascript" src="js/jquery-bgpos.js"></script>
+
+<script>
+$(function() {
+	$( "#tabs" ).tabs();
+});
+</script>
+
+</head>
+
+<body>
+<div id="google_translate_element"></div><script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'fr'
+  }, 'google_translate_element');
+}
+</script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+	<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="950">
+	<tbody>
+		<tr>
+			<td>&nbsp;</td>
+			<td align="center"><font color="#999999"><h1>rueil-petanque.fr</h1></font></td>
+			<td>&nbsp;</td>
+		</tr>  
+	 	 
+<!----------------------------------------------------------------- Bandeau ----------------------------------------------------------------------------->
+		<tr height="30">
+			<td colspan="4" height="50" valign="center">
+				<table align="center" width="95%">
+				<tbody>
+					<tr>
+						<td BGCOLOR="#afc8c7" align="left" width="15%"><a href="http://rueil-petanque.fr/index.php"><img src="imgs/boule.jpg" border="none" height="150" width="140"></a></td>
+						<td align="center" BGCOLOR="#afc8c7"><img src="imgs/drapeauf5.jpg" width="120" border="0"></td>
+						<td align="center" BGCOLOR="#afc8c7"><img src="imgs/blason.jpg" height="150" border="0"></td>
+						<td class="bann6" align="right"><i><b><strong color="black"> La pétanque dans la ville de Rueil-malmaison</strong>
+															<br>et/ou  à l'étranger : Slovaquie, Pologne, ...</b></i></td>
+					</tr>
+				</tbody>
+				</table>
+			
+		</tr>
+				
+<!------------------------------------------------------------- texte defilant --------------------------------------------------------------------------->
+		
+		<tr>
+			<td colspan="4" class="band6"><marquee scrollamount="2" behavior="scroll" direction="left"><i><b><meta http-equiv="content-type" content="text/html; charset=UTF-8">*** Concours Promotion Dimanche 8 mars à 14h00 La Verboise Garches : Doublette *** </b></i></marquee></td>
+		</tr>
+
+		<tr>
+			<td colspan="4" valign="top"> 
+				<br>
+				<table align="center" bgcolor="#000000" cellspacing="1" width="95%">
+				<tbody>
+					<tr height="30">
+						<td class="titre1h" valign="center" width='14%'  align='center'><a class="noir" href="#"><u>Accueil</u></a></td>
+						<td class="titre2h" valign="bottom" width='14%' align='center'><a  class="noir" href="menu2.php"  >Calendrier</a></td>
+						<td class="titre3h" valign="bottom" width='15%' align='center'><a  class="noir" href="menu3.php">Galerie</a></td> 
+						<td class="titre4h" valign="bottom" width='15%' align='center'><a  class="noir" href="menu5.php">Liens</a></td> 
+						<td class="titre5h" valign="bottom" width='14%' align='center'><a  class="noir" href="menu8.php"  >Les échos</a></td>
+						<td class="titre6h" valign="bottom" width='14%' align='center'><a  class="noir" href="menu4.php"  >Contact</a></td>
+						<td class="titre7h" valign="bottom" width='14%' align='center'><a  class="noir" href="menu7.php"  >Les Résultats</a></td>
+					</tr>
+					
+					
+				</table>
+				<table>
+					<div align="center"><p>&nbsp;</p>
+					
+						<a href="../Galerie/CHS/dimanche21062015/index.html"><h3>Coupe des Hauts de seine PCRM contre APR (Villeneuve-la-Garenne) "Cliquez ici".</h3></a><br>
+						<hr><hr>
+					
+						<a href="../Galerie/2anciens/dimanche8mars2015/index.html"><h3>Photos du Concours Pro du Dimanche 8 mars 2015 à la Verboise "Cliquez ici".</h3></a> <br>
+						 
+						<a href="../Galerie/2anciens/dimanche8mars2015suite/index.html"><h3>suite Photos du  à la Verboise "Cliquez ici".</h3></a> <br>
+						<hr><hr>
+						<a href="../Galerie/2anciens/samedi28022015/index.html"><h3>Photos du Concours du samedi 28 Février 2015  "Cliquez ici".</h3></a> <br>
+						<hr><hr>
+						<a href="../Galerie/2anciens/ASCO/index.html"><h3>Photos du  Concours du 26/02/2015 à L'ASCO "Cliquez ici".</h3></a><h3></a><br>
+						 
+				
+						<!--     <h1>Hommage à PASCAL MARIE</h1>
+						<h3>C'est avec une très grande tristesse que nous apprenons le décès de notre président <br></h3>
+							<img  src="Galerie/lacellesaintcloud/images/3.jpg" width="111">
+							<img  src="imgs/001_A.jpg" width="200">
+							<img  src="imgs/PM1.jpg" width="218"><br><br>
+							suite à une longue maladie,aimé et apprécié de tous pour sa gestion du club, son équité et son altruisme,<br> 
+							à l'origine du club d'abord appelé <strong>club des terres rouges</strong>, pour devenir aujourd'hui<strong> PCRM</strong>,<br>
+							, Il a su dynamiser  tout au long de ces années les différents tournois et concours, à l'écoute de chacun,<br>
+							et toujours avec bienveillance, c'était l'interlocuteur le plus respecté, apprécié et aimé de tous.<br>
+							C'était un excellent joueur de pétanque, et un Président omniprésent et très investi.<br><br>
+							
+							Toute notre sympathie pour Nicole son épouse, ses enfants et toute sa famille, à qui chacun d'entre nous adressons nos condoléances.
+							<br><br>   -->
+							
+					<div align="right">
+							 
+							<hr>
+							<div align="center">
+						 <br><br>
+						<h1>Demande d'ateliers de formation au PCRM</h1>
+						<h3>M Michel Giboreau propose une série de dix-huit ateliers<br>
+						pour aider chacun d'entre nous à progresser dans le domaine de la pétanque<br>
+						un nombre minimum de paticipants est requis,<br> je transmettrai ces demandes au Bureau du PCRM <br><br>
+						<a href="ressources/ateliers.pdf">Cliquez ICI pour visualiser, Télécharger ou imprimer ces différents ateliers</a></h3><br>
+						<h3><a href="../menu4.php">Cliquez ICI pour vous inscrire à ces derniers</a></h3>
+						<br><br>
+						<h1>Championnat des clubs 2014</h1>
+						<h3>Résultats des championnats des clubs 2ème division 2014</h3>
+						<p><strong>MONTENT EN 1ère DIVISION: P.C.R.M.1 ET CHARLY 4</strong><br></p>
+						<a href="../ressources/2final.xlsx">~ Télécharger au format Excel les Détails ~</a><br>
+						<p>Toutes nos félicitations à Nordine</p><br><br>
+						<h2>Résultats des championnats des clubs 4ème division 2014</h2>
+						<p><strong>Reste en 4ème DIVISION: P.C.R.M.2</strong>  </p><br><a href="../ressources/4eme.xlsx">~ Télécharger au format Excel les Détails ~</a>	 
+				
+				 <br><br><br>
+						<h1>Championnat Doublette Provençale 2014</h1>
+						<h3>Les CHAMPIONS :<br> BESSON Stéphane (PLESSIS ROBINSON)<br> et<br> KUBLER Patrick du PCRM</h2>
+						<p><strong>Toutes nos félicitations à Patrick et Stéphane</strong></p></h3>
+						 <img  src="imgs/DoubProv/1.jpg" width="400"><br><br>
+						 <img  src="imgs/DoubProv/2.jpg" width="400"><br>
+				
+				
+				</table>
+				<table>
+				</table>
+					<div align="center"><br><br><br>
+						<h1>Coupe de France 2014</h1>
+						<h3>Résultats des championnats de France<br>Triplette Sénior Masculin, à Brive la Gaillarde :</h3>
+						<p>Victoire en finale, 13 à 9, de la triplette Berlier-Rouquie-Ferrand (Midi-Pyrénées) face à Lacroix Le Boursicaud-Loy.<br></p>
+						<img  src="imgs/coupeFrance2014/capture2.png" width="400"><img  src="imgs/coupeFrance2014/finaliste2014.jpg" width="400"><br><br>
+						<img  src="imgs/coupeFrance2014/capture.png" width="800"><br>
+						<img  src="imgs/coupeFrance2014/demi.jpg" width="450"><br>
+						<img  src="imgs/coupeFrance2014/capture4.png" width="800"><br>
+						<img  src="imgs/coupeFrance2014/quart.jpg" width="450"><br>
+						
+						<p>En 2013 à Bratislava (Slovaquie), <strong>Francis Leydet</strong> (demi-finaliste de la coupe de France 2014)<br>
+						et <strong>Michel Giboreau</strong> du PCRM de Rueil-Malmaison, avaient perdus en 1/4 de finale.<br> Nous espérons retrouver Francis à la prochaine finale de cette coupe de France,<br>
+						et lui adressons à lui et ses deux partenaires (<strong>Frédéric Bodet et Kader Benefissa</strong>) toutes nos félicitations.<br>(Michel Giboreau et le PCRM)</p>
+					</div>
+					<br><br>
+					 
+	</table>
+	 
+
+</body></html>
